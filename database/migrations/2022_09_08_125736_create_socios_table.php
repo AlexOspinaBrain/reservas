f0616ed6a9de05be8b5038fae8ca3ead6cd5c8c3
Alex Ospina <eiji2l@gmail.com>
2022-09-08 08:13:45 -0500

    set migrations and models

---
+++ database/migrations/2022_09_08_125736_create_socios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('socios', function (Blueprint $table) {
+            $table->id();
+            $table->integer('identificacion')->nullable(false);
+            $table->string('nombre')->nullable(false);
+            $table->string('apellido')->nullable(false);
+            $table->boolean('estado')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('socios');
+    }
+};
---
+++ database/migrations/2022_09_08_130317_create_reservas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('socio_id')->references('id')->on('socios');
+            $table->foreignId('funcion_id')->references('id')->on('funciones');
+            $table->string('silla');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservas');
+    }
+};
